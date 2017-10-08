01f82e143ecf515f88c27c438b404e8ec8424868
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-10-08 12:38:22 -0400

    Add webapp

---
+++ webapp.py
@@ -0,0 +1,14 @@
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic()
+
+
+@app.route("/")
+async def index():
+    """ """
+    return text("Hello world")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, workers=2)
