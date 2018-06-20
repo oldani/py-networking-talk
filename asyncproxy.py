dbe19f3e83ab282a940c47fc71176744ec6b5042
Ordanis Sanchez <ordanisanchez@gmail.com>
2018-06-20 19:10:30 -0400

    Add asyncproxy

--- asyncproxy.py
+++ asyncproxy.py
@@ -1,38 +1,80 @@
 import asyncio
-from urllib.parse import urlparse
-import socket
+from utils import get_headers_data
 
 
 class Client(asyncio.Protocol):
     loop = None
+    CRLF = "\r\n"
+    buffersize = 8192
 
     def connection_made(self, transport):
         self.transport = transport
 
+    def connection_lost(self, excep):
+        """ """
+        if excep:
+            print(excep)
+        self.transport.close()
+        if hasattr(self, "connection"):
+            self.connection.close()
+
     def data_received(self, data):
-        print(data)
-        data = data.decode().split("\r\n")
-        host = data[0].split()[1]
-        print(host)
-        host = urlparse(host)
-        print(host)
-        netloc = host.netloc or host.path
-        host, port = netloc.split(":")
-        port = int(port)
-        asyncio.ensure_future(self.co(host, port, data))
+        """ """
+        self.data = data
+        address, method, self.version = get_headers_data(data)
+        asyncio.ensure_future(self.handle_connection(address, method))
 
-    async def co(self, host, port, data):
-        r, w = await asyncio.open_connection(host=host, port=port, loop=self.loop)
-        dd = "\r\n".join(data)
+    async def handle_connection(self, address, method):
+        """ """
+        print(address, method, self.data)
+        mname = f"do_{method}"
+        if not hasattr(self, mname):
+            return
+        method = getattr(self, mname)
+        try:
+            reader, self.writer = await asyncio.open_connection(host=address[0],
+                                                                port=address[1],
+                                                                loop=self.loop)
+        except Exception as e:
+            print(e)
+        await method(reader, self.writer)
 
-        w.write(dd.encode())
-        await w.drain()
+    async def do_CONNECT(self, reader, writer):
+        """ """
+        out = (f"HTTP/{'.'.join(str(n) for n in self.version)} "
+               f"200 Connection established{self.CRLF * 2}".encode())
+        self.transport.write(out)
+        self.connection = self.transport.get_extra_info('socket')
+        self.loop.add_reader(self.connection, self.read_write, writer)
+        data = await reader.read(n=self.buffersize)
+        self.transport.write(data)
+        self.loop.add_reader(self.connection, self.read_write, writer)
+
+    def read_write(self, writer):
+        """ """
+        asyncio.ensure_future(self._read_write(writer))
+
+    async def _read_write(self, writer):
+        """ """
         while True:
-            d = await r.read(n=8192)
-            if not d:
+            data = await self.loop.sock_recv(self.connection, self.buffersize)
+            if not data:
                 break
-            self.transport.write(d)
-        w.close()
+
+            writer.write(data)
+            await writer.drain()
+        self.connection_lost(None)
+
+    async def do_GET(self, reader, writer):
+        """ """
+        writer.write(self.data)
+        await writer.drain()
+        while True:
+            data = await reader.read(n=8192)
+            if not data:
+                break
+            self.transport.write(data)
+        writer.close()
         self.transport.close()
 
 
